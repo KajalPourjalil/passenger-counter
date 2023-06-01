42a2514b5fb49ff675620a755ed17e6bbe817cff
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-06-01 12:16:19 +0300

    js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+//store data in a var
+let count = 0;
+console.log(count);
+
+let myAge = 24;
+console.log(myAge);
