6013a91b431c0d6776c21498ed9648c631717382
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-06-02 12:23:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ console.log(count);
 
 let myAge = 24;
 console.log(myAge);
+
+let firstBatch = 5
+let secondBatch = 6;
+count = firstBatch
+console.log(firstBatch); 
+console.log(firstBatch + secondBatch);
