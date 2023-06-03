8e1e0896a2f9993ccec037fbc9087872c046c0e3
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-06-03 12:25:23 +0300

    js

--- index.js
+++ index.js
@@ -2,13 +2,8 @@
 
 //store data in a var
 let count = 0;
-console.log(count);
+
 
 let myAge = 24;
-console.log(myAge);
-
-let firstBatch = 5
-let secondBatch = 6;
-count = firstBatch
-console.log(firstBatch); 
-console.log(firstBatch + secondBatch);
+let humanDogRatio = 3;
+console.log(myAge * humanDogRatio);
