7564500f5a7f79dc94a6489f2b6f5c3f80dfbf27
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-06-04 12:35:06 +0300

    js

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
 //store data in a var
 let count = 0;
 
+let bonusPoints = 50;
 
-let myAge = 24;
-let humanDogRatio = 3;
-console.log(myAge * humanDogRatio);
+bonusPoints = bonusPoints * 2;
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints / 4;
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints * 3;
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 5
+
+console.log(bonusPoints)
