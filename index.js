7a792ac11c5618c46c9127d6d9c87decb5e9f26f
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-06-05 12:41:31 +0300

    button

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// document.getElementById("count-el").innerText = 5;
+document.getElementById("count-el").innerText = 5;
 
 //store data in a var
-let count = 0;
-
-let bonusPoints = 50;
-
-bonusPoints = bonusPoints * 2;
-
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints / 4;
-
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints * 3;
-
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 5
-
-console.log(bonusPoints)
