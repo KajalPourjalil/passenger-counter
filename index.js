05e81e8b5ad10034f9e5b4e1df06c6c09904002f
KajalPourjalil <rina.skyler@yahoo.com>
2023-08-04 12:44:05 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-// document.getElementById("increment-btn").onclick
 
-//store data in a var
+let count = 0;
+function increment() {
+//     // console.log("clikced")
+    count += 1;
+    document.getElementById("count-el").innerHTML = count
+    console.log(count);
+    
+}
 
-// function increment() {
-//     console.log('button was clicked')
-//     document.getElementById("")
+// increment();
+
+// let countEl = document.getElementById("count-el");
+
+// function increment2() {
+//     count+=1;
+//     countEl.innerText = count
 // }
 
-let lap1 = 34;
-let lap2 = 33;
-let lap3 = 36;
-
-function now() {
-    console.log(lap1 + lap2 + lap3);
-}
-
-now();
-
-let laps = 0;
-
-function increment() {
-    console.log(laps++)
-}
-
-increment();
-increment();
 increment();
